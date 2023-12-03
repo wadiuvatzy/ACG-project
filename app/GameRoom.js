01f7c7687d13673495b31bec3847c695c4849c9a
wadiuvatzy <ziyu-wan21@mails.tsinghua.edu.cn>
2023-12-03 14:28:49 +0800

    add reset text after death

--- app/GameRoom.js
+++ app/GameRoom.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import * as utils from '../utils';
 import * as game_objects from '../objects';
 import * as levels from '../levels';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 class GameRoom {
 	constructor() {
@@ -14,6 +16,8 @@ class GameRoom {
 		this.blocks = [];
 		this.spikes = [];
 		this.special_objects = [];
+		this.dead_waiting_reset = false;
+		this.just_dead = false;
 	}
 	init_debug() {
 		/*
@@ -33,23 +37,22 @@ class GameRoom {
 		this.spikes.push(new game_objects.Spike(this, new THREE.Vector2(-10, 40), null));
 		this.spikes.push(new game_objects.Spike(this, new THREE.Vector2(40, 30), this.blocks[2]));
 		*/
-		
+
 		levels.make_level(this, levels.NameToLevel["Tutorial_easy1"]);
 
 		// add lights
-		const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-		directionalLight.position.x = 10;
-		directionalLight.position.y = 100;
-		directionalLight.position.z = 20;
-		this.scene.add(directionalLight);
+		this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+		this.directionalLight.position.x = 10;
+		this.directionalLight.position.y = 100;
+		this.directionalLight.position.z = 20;
+		this.scene.add(this.directionalLight);
 
 		const ambientlight = new THREE.AmbientLight(0x404040); // soft white light
 		this.scene.add(ambientlight);
 
-		this.camera.position.x = 152;
+		this.camera.position.x = 100;
 		this.camera.position.y = 64;
 		this.camera.position.z = 200;
-
 		this.reset();
 	}
 	reset() {
@@ -67,6 +70,16 @@ class GameRoom {
 
 		this.paused = false;
 		this.wins = false;
+		this.dead_waiting_reset = false;
+
+		this.scene.remove(this.directionalLight)
+		this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+		this.directionalLight.position.x = 10;
+		this.directionalLight.position.y = 100;
+		this.directionalLight.position.z = 20;
+		this.scene.add(this.directionalLight);
+		this.scene.remove(this.reset_text);
+
 	}
 	// Given the time elapsed and keyboard inputs, compute the next state.
 	Step() {
@@ -81,6 +94,14 @@ class GameRoom {
 			}
 			else if (this.player.should_be_killed) {
 				// TODO (may be nothing to do here)
+				if (this.dead_waiting_reset) {
+					this.just_dead = false;
+					this.dead_waiting_reset = true;
+				}
+				else {
+					this.just_dead = true;
+					this.dead_waiting_reset = true;
+				}
 				return;
 			}
 			if (keyboardValue.PausePressed) {
@@ -119,6 +140,41 @@ class GameRoom {
 			obj.onRender();
 		}
 		this.player.onRender();
+		if (this.just_dead) {
+			this.scene.remove(this.directionalLight)
+			// window.alert("scene: ", this.scene)
+			this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
+			this.directionalLight.position.x = 10;
+			this.directionalLight.position.y = 100;
+			this.directionalLight.position.z = 20;
+			this.scene.add(this.directionalLight);
+
+			// add text to tell player to press R to reset
+			this.scene.remove(this.reset_text);
+			const loader = new FontLoader();
+			loader.load('../fonts/helvetiker_regular.typeface.json', function (font) {
+				var geometry = new TextGeometry("Press R to reset", {
+					font: font,
+					size: 12,
+					height: 1,
+					bevelEnabled: true,
+					bevelThickness: 0.5,
+					bevelSize: 0.3,
+					bevelSegments: 0.5
+				});
+				geometry.computeBoundingBox();
+				geometry.center();
+				var material = new THREE.MeshStandardMaterial({ color: 0x00ffff });
+				material.emissive = new THREE.Color(0x00ffff);
+				material.emissiveIntensity = 0.5;
+				this.reset_text = new THREE.Mesh(geometry, material);
+				this.reset_text.position.x = this.camera.position.x;
+				this.reset_text.position.y = this.camera.position.y;
+				this.reset_text.position.z = this.camera.position.z - 100;
+				this.scene.add(this.reset_text);
+			}.bind(this));
+
+		}
 	}
 }
 
